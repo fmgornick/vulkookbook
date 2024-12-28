54f4646a170c269b9438e7ec389527760d0b7007
Fletcher Gornick <fletcher.gornick@target.com>
2024-12-28 15:10:31 -0600

    chapter 1: queue families

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #define VK_NO_PROTOTYPES
+// #define DEBUG
 
+#include <assert.h>
 #include <dlfcn.h>
+#include <iso646.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <vulkan/vk_enum_string_helper.h>
 #include <vulkan/vulkan_core.h>
 
@@ -18,6 +22,12 @@
 #define lib_addr dlsym
 #endif
 
+#ifdef DEBUG
+#define DBG(fmt, args...) fprintf(stderr, fmt, ##args)
+#else
+#define DBG(fmt, args...) /* Don't do anything in release builds */
+#endif
+
 // library
 void *vulkan_library;
 
@@ -27,17 +37,38 @@ VkInstance instance;
 // exported functions
 PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr;
 
-// global level functions
+// global-level functions
 PFN_vkCreateInstance vkCreateInstance;
 PFN_vkEnumerateInstanceExtensionProperties vkEnumerateInstanceExtensionProperties;
 PFN_vkEnumerateInstanceLayerProperties vkEnumerateInstanceLayerProperties;
 
-// instance level functions
+// instance-level functions
 PFN_vkCreateDevice vkCreateDevice;
+PFN_vkEnumerateDeviceExtensionProperties vkEnumerateDeviceExtensionProperties;
 PFN_vkEnumeratePhysicalDevices vkEnumeratePhysicalDevices;
 PFN_vkGetDeviceProcAddr vkGetDeviceProcAddr;
+PFN_vkGetDeviceQueue vkGetDeviceQueue;
 PFN_vkGetPhysicalDeviceFeatures vkGetPhysicalDeviceFeatures;
 PFN_vkGetPhysicalDeviceProperties vkGetPhysicalDeviceProperties;
+PFN_vkGetPhysicalDeviceQueueFamilyProperties vkGetPhysicalDeviceQueueFamilyProperties;
+
+// instance-level extensions
+// VK_EXT_debug_utils
+PFN_vkCmdBeginDebugUtilsLabelEXT vkCmdBeginDebugUtilsLabelEXT;
+PFN_vkCmdEndDebugUtilsLabelEXT vkCmdEndDebugUtilsLabelEXT;
+PFN_vkCmdInsertDebugUtilsLabelEXT vkCmdInsertDebugUtilsLabelEXT;
+PFN_vkCreateDebugUtilsMessengerEXT vkCreateDebugUtilsMessengerEXT;
+PFN_vkDestroyDebugUtilsMessengerEXT vkDestroyDebugUtilsMessengerEXT;
+PFN_vkQueueBeginDebugUtilsLabelEXT vkQueueBeginDebugUtilsLabelEXT;
+PFN_vkQueueEndDebugUtilsLabelEXT vkQueueEndDebugUtilsLabelEXT;
+PFN_vkQueueInsertDebugUtilsLabelEXT vkQueueInsertDebugUtilsLabelEXT;
+PFN_vkSetDebugUtilsObjectNameEXT vkSetDebugUtilsObjectNameEXT;
+PFN_vkSetDebugUtilsObjectTagEXT vkSetDebugUtilsObjectTagEXT;
+PFN_vkSubmitDebugUtilsMessageEXT vkSubmitDebugUtilsMessageEXT;
+// VK_EXT_debug_report
+PFN_vkCreateDebugReportCallbackEXT vkCreateDebugReportCallbackEXT;
+PFN_vkDebugReportMessageEXT vkDebugReportMessageEXT;
+PFN_vkDestroyDebugReportCallbackEXT vkDestroyDebugReportCallbackEXT;
 
 int main(int argc, char *argv[]) {
     vulkan_library = lib_open("libvulkan.dylib");
@@ -56,31 +87,37 @@ int main(int argc, char *argv[]) {
 #define GLOBAL_LEVEL_VULKAN_FUNCTION(name)                                 \
     name = (PFN_##name)vkGetInstanceProcAddr(NULL, #name);                 \
     if (name == NULL) {                                                    \
-        printf("could not load global level vulkan function %s\n", #name); \
+        printf("could not load global-level vulkan function %s\n", #name); \
         return 1;                                                          \
     }
 
 #include "vulkan_functions.inl"
 
-    uint32_t extensions_count = 0;
-    VkResult result = VK_SUCCESS;
+    uint32_t extensions_count;
+    VkResult result;
     result = vkEnumerateInstanceExtensionProperties(NULL, &extensions_count, NULL);
     if (result != VK_SUCCESS || extensions_count == 0) {
         printf("could not get instance extensions: %s\n", string_VkResult(result));
         return 1;
     }
 
-    VkExtensionProperties *available_extensions = malloc(sizeof(VkExtensionProperties) * extensions_count);
-    vkEnumerateInstanceExtensionProperties(NULL, &extensions_count, available_extensions);
+    VkExtensionProperties *available_extensions = (VkExtensionProperties *)malloc(sizeof(VkExtensionProperties) * extensions_count);
+    result = vkEnumerateInstanceExtensionProperties(NULL, &extensions_count, available_extensions);
     if (result != VK_SUCCESS || extensions_count == 0) {
         printf("could not get instance extensions: %s\n", string_VkResult(result));
         return 1;
     }
 
+#ifdef DEBUG
+    printf("INSTANCE EXTENSIONS\n");
+    for (int i = 0; i < extensions_count; i++) printf("%s\n", available_extensions[i].extensionName);
+    printf("\n");
+#endif
+
     const char *desired_extensions[] = {
-        "VK_KHR_portability_enumeration",
         "VK_EXT_debug_report",
         "VK_EXT_debug_utils",
+        "VK_KHR_portability_enumeration",
     };
 
     VkApplicationInfo application_info = {
@@ -113,11 +150,93 @@ int main(int argc, char *argv[]) {
 #define INSTANCE_LEVEL_VULKAN_FUNCTION(name)                                 \
     name = (PFN_##name)vkGetInstanceProcAddr(instance, #name);               \
     if (name == NULL) {                                                      \
-        printf("could not load instance level vulkan function %s\n", #name); \
+        printf("could not load instance-level vulkan function %s\n", #name); \
         return 1;                                                            \
     }
 
+#define INSTANCE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION(name, extension)                \
+    for (int i = 0; i < extensions_count; i++) {                                      \
+        if (strcmp(#extension, available_extensions[i].extensionName) == 0) {         \
+            name = (PFN_##name)vkGetInstanceProcAddr(instance, #name);                \
+            if (name == NULL) {                                                       \
+                printf("could not load instance-level vulkan extension %s\n", #name); \
+                return 1;                                                             \
+            }                                                                         \
+        }                                                                             \
+    }
+
 #include "vulkan_functions.inl"
+
+    uint32_t device_count;
+    result = vkEnumeratePhysicalDevices(instance, &device_count, NULL);
+    if (result != VK_SUCCESS || device_count == 0) {
+        printf("could not enumerate physical devices: %s\n", string_VkResult(result));
+        return 1;
+    }
+
+    VkPhysicalDevice *available_devices = (VkPhysicalDevice *)malloc(sizeof(VkPhysicalDevice) * device_count);
+    result = vkEnumeratePhysicalDevices(instance, &device_count, available_devices);
+    if (result != VK_SUCCESS || device_count == 0) {
+        printf("could not enumerate physical devices: %s\n", string_VkResult(result));
+        return 1;
+    }
+
+    assert(device_count == 1);
+    VkPhysicalDevice device = available_devices[0];
+    VkPhysicalDeviceProperties device_properties;
+    VkPhysicalDeviceFeatures device_features;
+    vkGetPhysicalDeviceProperties(device, &device_properties);
+    vkGetPhysicalDeviceFeatures(device, &device_features);
+
+    uint32_t extension_count;
+    result = vkEnumerateDeviceExtensionProperties(device, NULL, &extension_count, NULL);
+    if (result != VK_SUCCESS || extension_count == 0) {
+        printf("could not enumerate device extensions: %s\n", string_VkResult(result));
+        return 1;
+    }
+
+    VkExtensionProperties *extension_properties = (VkExtensionProperties *)malloc(sizeof(VkExtensionProperties) * extension_count);
+    result = vkEnumerateDeviceExtensionProperties(device, NULL, &extension_count, extension_properties);
+    if (result != VK_SUCCESS || extension_count == 0) {
+        printf("could not enumerate device extensions: %s\n", string_VkResult(result));
+        return 1;
+    }
+
+#ifdef DEBUG
+    printf("DEVICE EXTENSIONS (%s)\n", device_properties.deviceName);
+    for (int i = 0; i < extension_count; i++) printf("%s\n", extension_properties[i].extensionName);
+    printf("\n");
+#endif
+
+    uint32_t queue_families_count;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_families_count, NULL);
+    if (queue_families_count == 0) {
+        printf("could not get device queue properties\n");
+        return 1;
+    }
+
+    VkQueueFamilyProperties *queue_familes =
+        (VkQueueFamilyProperties *)malloc(sizeof(VkQueueFamilyProperties) * queue_families_count);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_families_count, queue_familes);
+    if (queue_families_count == 0) {
+        printf("could not get device queue properties\n");
+        return 1;
+    }
+
+    uint32_t queue_family_index;
+    VkQueueFlags desired_capabilities = VK_QUEUE_GRAPHICS_BIT;
+    for (uint32_t i = 0; i < queue_families_count; i++) {
+        VkQueueFamilyProperties queue_family = queue_familes[i];
+        if (queue_family.queueCount > 0 && queue_family.queueFlags & desired_capabilities) queue_family_index = i;
+    }
+
+    // VkQueue queue;
+    // vkGetDeviceQueue(device, queue_family_index, 0, &queue);
+
+    free(queue_familes);
+    free(extension_properties);
+    free(available_extensions);
+    free(available_devices);
 
     return 0;
 }
